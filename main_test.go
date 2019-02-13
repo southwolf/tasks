8b5f37af977b2018634030d3db70813eee5c3654
southwolf <wp.southwolf@gmail.com>
2019-02-13 23:06:06 +0800

    Get tasks, create task and get task with tests
    TODO: refactor test code to extract duplication

--- main_test.go
+++ main_test.go
@@ -3,26 +3,57 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
-	"os"
+	"strings"
 	"testing"
+
+	"github.com/gorilla/mux"
 )
 
-func TestTasksHandler(t *testing.T) {
-	request, err := http.NewRequest("GET", "/tasks", nil)
+func TestCreateTasks(t *testing.T) {
+	taskPayload := `{"id":1,"title":"Hello world!","done":false}`
+	request, err := http.NewRequest("POST", "/tasks", strings.NewReader(taskPayload))
 	if err != nil {
 		t.Fatal(err)
 	}
 	response := httptest.NewRecorder()
-	handler := http.HandlerFunc(TasksHandler)
+	handler := http.HandlerFunc(CreateTask)
 	handler.ServeHTTP(response, request)
 
-	expected := "You've reached tasks routes"
-	actual := response.Body.String()
+	expected := taskPayload
+	actual := strings.TrimSpace(response.Body.String())
 	if actual != expected {
 		t.Errorf("Wrong response: got %v want %v", actual, expected)
 	}
 }
 
-func TestMain(m *testing.M) {
-	os.Exit(m.Run())
+func TestGetTasks(t *testing.T) {
+	request, err := http.NewRequest("GET", "/tasks", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	response := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetTasks)
+	handler.ServeHTTP(response, request)
+
+	expected := `[{"id":1,"title":"Hello world!","done":false}]`
+	actual := strings.TrimSpace(response.Body.String())
+	if actual != expected {
+		t.Errorf("Wrong response: got %v want %v", actual, expected)
+	}
+}
+
+func TestGetTask(t *testing.T) {
+	routes := mux.NewRouter()
+	routes.HandleFunc("/tasks/{id}", GetTask)
+
+	taskPayload := `{"id":1,"title":"Hello world!","done":false}`
+	request, _ := http.NewRequest("GET", "/tasks/1", nil)
+	response := httptest.NewRecorder()
+	routes.ServeHTTP(response, request)
+
+	expected := taskPayload
+	actual := strings.TrimSpace(response.Body.String())
+	if actual != expected {
+		t.Errorf("Wrong response: got %v want %v", actual, expected)
+	}
 }
